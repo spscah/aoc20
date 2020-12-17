56f563b5746fafcbd3675f391d768b9207532d65
Chris Harrison <cah@stpaulsschool.org.uk>
2020-12-17 12:01:13 +0000

    day 17 - v0.1 crucifyingly slow

--- day17/ConwayCubes.App/Program.cs
+++ day17/ConwayCubes.App/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace ConwayCubes.App
 {
@@ -11,6 +11,7 @@ namespace ConwayCubes.App
             IList<string> cycle0 = new List<string> { ".#.", "..#", "###"};
             
             Cubes c = new Cubes(cycle0, 6);
+            c.Go();
             Console.WriteLine(c.NumberActive);
             Console.WriteLine("works for me");
         }
@@ -18,14 +19,16 @@ namespace ConwayCubes.App
 
     } 
 
+
+
     class Cubes {
         readonly int _initialSize;
         readonly int _cycles;
         IList<bool> _state; 
 
-        int XDim => 2*_initialSize+_cycles;
+        int XDim => 2*_cycles+_initialSize;
         int YDim => XDim;
-        int ZDim => 2*_initialSize+1;
+        int ZDim => 2*_cycles+1;
 
         public int NumberActive => _state.Count(c => c == true);
 
@@ -33,14 +36,73 @@ namespace ConwayCubes.App
         {
             _cycles = c;
             _initialSize = initialState[0].Length;
-            _state = Enumerable.Range(0,XDim*YDim*ZDim).Select(r => false).ToList();
+            _state = CleanSlate();
             int z = 0;
             for(int y=0; y < initialState.Count; ++y)
                 for(int x=0; x < initialState[0].Length; ++x)
                     if(initialState[y][x] == '#')
-                        _state[Transform(x,y,z)] = true;
+                        _state[Transform(x+c,y+c,z+c)] = true;
+            Console.WriteLine(NumberActive);
+
+        }
+
+        public int Go()
+        {
+            for(int i = 0; i < _cycles; ++i)
+                Step(i+1);
+            return NumberActive;
+        }
+
+        IList<bool> CleanSlate() 
+        {
+            return Enumerable.Range(0,XDim*YDim*ZDim).Select(r => false).ToList();
+        }
+        void Step(int offset)
+        {
+            IList<bool> newState = CleanSlate();
+            for(int x = _cycles - offset; x  < XDim - _cycles + offset; ++x) {
+                for(int y = _cycles - offset; y < YDim- _cycles + offset; ++y) {
+                    for(int z = _cycles - offset; z < ZDim- _cycles + offset; ++z) {
+                        int i = Transform(x,y,z);
+                        int count = Neighbours(x,y,z).Where(n => _state[n]).Count();
+                        if(_state[i]) 
+                            newState[i] = (count == 2 || count == 3);
+                        else    
+                            newState[i] = count == 3; 
+                    }
+                }
+            }
+            _state = newState;
+            Console.WriteLine("{0}: {1}", offset, NumberActive);
+        }
 
 
+
+        IEnumerable<int> Neighbours(int x, int y, int z)
+        {
+            int origin = Transform(x,y,z);
+            foreach(int ox in new List<int> { -1, 0, 1}) {
+                if(x + ox < 0)
+                    continue;
+                if(x + ox >= XDim)
+                    continue;
+                foreach(int oy in new List<int> { -1, 0, 1}) {
+                    if(y + oy < 0)
+                        continue;
+                    if(y + oy >= YDim)
+                        continue;
+                    foreach(int oz in new List<int> { -1, 0, 1}) {
+                        if(z + oz < 0)
+                            continue;
+                        if(z + oz >= ZDim)
+                            continue;
+                        if(ox == 0 && oy == 0 && oz == 0) 
+                            continue;
+                        int rv = Transform(x+ox, y+oy, z+oz);
+                        yield return rv;                        
+                    }
+                }
+            }                        
         }
 
         int Transform(int x, int y, int z) {
@@ -48,11 +110,11 @@ namespace ConwayCubes.App
         }
 
         int YTransform(int y) {
-            return (y + _cycles) * XDim;
+            return y * XDim;
         }
 
         int ZTransform(int z) {
-            return (z + _cycles) * XDim * YDim;
+            return z * XDim * YDim;
         }
     }
 }
